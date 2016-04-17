00ac26264bf50372e3d2bc85fede802c8832edce
Garrett Credi <gcc@ameritech.net>
2016-04-17 13:28:34 -0500

    Adding to both Git and Heroku

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ var app = angular.module('pristineProjects', []);
 app.directive('navbarPristine', function(){
   return{
     restrict: 'E',
-    templateUrl: '../pages/navbar.html'
+    templateUrl: 'http://pristine-projects.herokuapp.com/pages/navbar.html'
   }
 });
